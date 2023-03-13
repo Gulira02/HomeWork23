5aaa0f5da1d9ca264ee6f1dc5a5ee9232ec46857
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-03-13 22:52:58 +0600

    addEvenNumbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random=new Random();
+        List<Integer> arrayList=new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            int number = random.nextInt(100) + 1;
+            arrayList.add(number);
+           // System.out.print(" "+number);
+        }
+        List <Integer>oddNums=new ArrayList<>();
+        List <Integer>evNums=new ArrayList<>();
+        for (int num:arrayList) {
+            if(num%2==0){
+                evNums.add(num);
+                System.out.println("Четное число: "+num);
+            }
+            else{
+                oddNums.add(num);
+                System.out.println("Нечетное число: "+num);
+            }
+        }
+        
+
     }
 }
